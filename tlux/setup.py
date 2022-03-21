d40a89dd69e71d8c112c8db6acba75af943408a8
Thomas Lux <thomas.ch.lux@gmail.com>
2022-03-20 18:35:38 -0700

    Iterating on setup for compiling Fortran codes. Output directory was wrong.

--- tlux/setup.py
+++ tlux/setup.py
@@ -4,10 +4,14 @@
 # setup should go here to prepare the module on a new computer.
 
 # Try compiling the 'fmath' library.
+import sys
+_first = sys.path.pop(0)
 try:
     import os, fmodpy
-    _here = os.path.dirname(os.path.abspath(__file__))
+    _here = os.path.join(os.path.dirname(os.path.abspath(__file__)), "math")
     _fmath_path = os.path.join(_here, "fmath.f90")
-    _fmath = fmodpy.fimport(_fmath_path, verbose=False, output_dir=_here)
-except:
-    pass
+    _fmath = fmodpy.fimport(_fmath_path, lapack=True, verbose=False, output_dir=_here)
+except Exception as exc:
+    print(f"WARNING: 'tlux' encountered exception while trying to compile the 'fmath' library.")
+finally:
+    sys.path.insert(0, _first)
