181b41ec0b5a81f012ecf93033b89c8a7f989aab
Thomas Lux <thomas.ch.lux@gmail.com>
2024-04-28 13:29:03 -0700

    Added new 'tlux.network' module with a 'NetworkQueue' that behaves like a regular 'Queue' but sends data serialized over network connections potentially between different host machines. Included basic local serialized test and a basic network test.

---
+++ tlux/network.py
@@ -0,0 +1,210 @@
+import logging
+import pickle
+import socket
+import threading
+from queue import Queue
+from typing import Any, Callable, Optional
+
+
+# Use pickle to serialize data.
+def PICKLE_DUMPS(o: Any) -> bytes:
+    return pickle.dumps(o, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+# Use pickle to load serialized data.
+# WARNING: Generally unsafe.
+def PICKLE_LOADS(o: bytes) -> Any:
+    return pickle.loads(o)
+
+
+# Get the IP address of this host, otherwise return the host name.
+def get_ip():
+    try:
+        # Get the fully qualified local hostname
+        hostname = socket.getfqdn()
+        # Resolve the hostname to an IP address
+        ip = socket.gethostbyname(hostname)
+    except Exception as e:
+        # Otherwise just use the host name.
+        ip = socket.gethostname()
+    logging.info(f"network.NetworkQueue local IP {repr(ip)}")
+    return ip
+
+
+# This class provides the functionality of a "Queue" that works across multiple hosts using network connections.
+class NetworkQueue:
+    def __init__(
+            self,
+            host: Optional[str] = None,
+            port: Optional[int] = None,
+            listen_host: Optional[str] = None,
+            listen_port: Optional[int] = 0,
+            serialize: Callable[[Any],bytes] = PICKLE_DUMPS,
+            deserialize: Callable[[bytes],Any] = PICKLE_LOADS,
+    ):
+        # Create a holder for clients, queues that contains incoming data from client connections.
+        self.active = True
+        self.exit_event = threading.Event()
+        self.clients = {}
+        self.queues = {}
+        self.arrivals = Queue()
+        self.popped = []
+        self.serialize = serialize
+        self.deserialize = deserialize
+
+        # Set the host name and port of the other NetworkQueue.
+        self.host = host
+        self.port = port
+
+        # If we want to listen for outside connections, then do that.
+        if (listen_port is not None):
+            self.listener_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.listener_host = listen_host or get_ip()
+            self.listener_socket.bind((self.listener_host, listen_port))
+            self.listener_port = self.listener_socket.getsockname()[1]  # Retrieves the automatically assigned port if 0.
+            self.listener_socket.listen()
+            # Start a thread that listens for incoming connections.
+            self.listener_thread = threading.Thread(target=self._accept_connections, daemon=True)
+            self.listener_thread.start()
+            logging.info(f"network.NetworkQueue listening at {self.listener_host}:{self.listener_port}")
+
+        # Connect to the other NetworkQueue.
+        if ((host is not None) and (port is not None)):
+            logging.info(f"network.NetworkQueue establishing connection with {host}:{port}")
+            host_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            host_socket.connect((host, port))
+            self.clients["host"] = (
+                host_socket,
+                threading.Thread(target=self._client_listener, args=(host_socket, "host"), daemon=True)
+            )
+            # Start the dedicated host communications listener.
+            self.clients["host"][1].start()
+            logging.info(f"network.NetworkQueue connected to host {host}:{port}")
+
+
+    # Listen for connections from other NetworkQueue objects.
+    def _accept_connections(self):
+        while (not self.exit_event.is_set()):
+            try:
+                client_socket, addr = self.listener_socket.accept()
+                if self.exit_event.is_set():
+                    client_socket.close()
+                    break
+                client_id = addr[1]  # Using the client's port as a simple ID
+                # Store the socket and the communication thread for this client.
+                self.clients[client_id] = (
+                    client_socket,
+                    threading.Thread(target=self._client_listener, args=(client_socket, client_id), daemon=True)
+                )
+                # Start a dedicated client communications listener.
+                self.clients[client_id][1].start()
+                logging.info(f"network.NetworkQueue connected to client {repr(client_id)} at {addr}")
+            except socket.error:
+                break
+
+
+    # Establishes a dedicated Queue for a client and listens for communications.
+    def _client_listener(self, client_socket: socket.socket, client_id: int):
+        q = Queue()
+        self.queues[client_id] = q
+        while (not self.exit_event.is_set()):
+            try:
+                # First listen for an integer that specifies the size of the data.
+                data = client_socket.recv(8)
+                if (data):
+                    # Then decode that integer listen for the object that follows.
+                    obj_size: int = int.from_bytes(data, "big")
+                    data = client_socket.recv(obj_size)
+                    if (data):
+                        logging.info(f"network.NetworkQueue object sized {obj_size} incoming from client {repr(client_id)}")
+                        # Put the received data into the client's designated queue stored locally.
+                        q.put(self.deserialize(data))
+                        # Indicate the arrival of data from this client in the parent arrivals queue.
+                        self.arrivals.put(client_id)
+                        logging.info(f"network.NetworkQueue data of size {obj_size} successfully arrived from client {repr(client_id)}")
+                    else:
+                        logging.error(f"network.NetworkQueue got size {obj_size} with no data following from client {repr(client_id)}")
+                elif (data == b''):
+                    logging.info(f"NetowrkQueue received data b'' from client {repr(client_id)} indicating a closed connection.")
+                    break
+                else:
+                    logging.error(f"network.NetworkQueue got data {data} that was invalid from client {repr(client_id)}")
+            # Socket errors trigger an immediate break.
+            except socket.error:
+                break
+            # In case of any errors, 
+            except Exception as e:
+                logging.error(f"network.NetworkQueue encountered error with client {repr(client_id)}: {e}")
+                break
+        # Close the connection.
+        client_socket.close()
+        # Remove this client from the tracked queues and clients.
+        self.queues.pop(client_id, None)
+        self.clients.pop(client_id, None)
+        logging.info(f"network.NetworkQueue Connection to client {client_id} closed")
+
+
+    # Send an item to clients (or a specific client if provided).
+    def put(self, item, client_id=None):
+        item = self.serialize(item)
+        size = len(item)
+        # If a client was provided, then send specifically to that one.
+        if (client_id in self.clients):
+            client_socket, client_thread = self.clients[client_id]
+            try:
+                client_socket.send(size.to_bytes(8, "big") + item)
+            except Exception as e:
+                logging.error(f"Failed to send item to client {repr(client_id)}: {e}")
+            logging.info(f"network.NetworkQueue sent 8 + {size} bytes to {repr(client_id)}")
+        # Otherwise, iterate over all clients and send them the item.
+        else:
+            for client_id, (client_socket, client_thread) in self.clients.items():
+                try:
+                    client_socket.send(size.to_bytes(8, "big") + item)
+                except Exception as e:
+                    logging.error(f"Failed to send item to client {repr(client_id)}: {e}")
+                logging.info(f"network.NetworkQueue sent to {repr(client_id)}")
+
+
+    # Get an item from clients (or a specific client if provided).
+    def get(self, client_id=None):
+        if (client_id in self.queues):
+            q = self.queues[client_id]
+            item = q.get()
+            self.popped.append(client_id)
+            return item
+        else:
+            client_id = self.arrivals.get()
+            while (client_id in self.popped):
+                self.popped.remove(client_id)
+                client_id = self.arrivals.get()
+            return self.queues[client_id].get()
+
+
+    # Close this Queue once it is done being used.
+    def close(self):
+        self.exit_event.set()
+        for (client_socket, client_thread) in self.clients.values():
+            client_socket.close()
+        if hasattr(self, 'server_socket'):
+            self.listener_socket.close()
+        logging.info("network.NetworkQueue shutdown completed")
+
+
+    # Deletion of this class should trigger everything to be closed.
+    def __del__(self):
+        self.close()
+
+
+    # Produce a string summarizing this NetworkQueue.
+    def __str__(self):
+        host = f""
+        if self.host is not None:
+            host = f"host-name={repr(self.host)} host-port={repr(self.port)} "
+        listener = f""
+        if hasattr(self, "listener_port"):
+            listener = f"listen-name={repr(self.listener_host)} listen-port={self.listener_port} "
+        clients = f""
+        if len(self.clients) > 0:
+            clients = f"client-count={len(self.clients)}"
+        return f"<NetworkQueue {listener}{host}{clients}>"
---
+++ tlux/tests/test_network.py
@@ -0,0 +1,61 @@
+# Set logging to be verbose.
+import logging
+logging.basicConfig(level=logging.INFO)
+
+# Running the NetworkQueue test.
+if __name__ == "__main__":
+    # Import the NetworkQueue object.
+    from tlux.network import NetworkQueue
+
+    # Offer a "listen-and-repeat" testing mode for across-network tests.
+    import sys
+    if "listen-and-repeat" in sys.argv:
+        queue = NetworkQueue(listen_port=64512)
+        result = queue.get()
+        while (result is not None):
+            queue.put(result)
+            result = queue.get()
+        exit()
+
+    # Offer a "ping-and-show" testing mode for across-network tests.
+    elif "ping-and-show" in sys.argv:
+        host = sys.argv[sys.argv.index("ping-and-show") + 1]
+        port = int(sys.argv[sys.argv.index("ping-and-show") + 2])
+        if (len(sys.argv) > sys.argv.index("ping-and-show") + 3):
+            message = sys.argv[sys.argv.index("ping-and-show") + 3]
+        else:
+            message = "Hello WORLD!"
+        queue = NetworkQueue(host=host, port=port, listen_port=None)
+        queue.put(message)
+        result = queue.get()
+        print(result, flush=True)
+        exit()
+
+    # Otherwise run a local test.
+
+    # Start server
+    server = NetworkQueue()
+
+    # Start a client (that does not listen for incoming connections).
+    client = NetworkQueue(host=server.listener_host, port=server.listener_port, listen_port=None)
+
+    # Put something into the server queue.
+    print("Sending from server to client..", flush=True)
+    phrase = "hello world!"
+    server.put(phrase)
+    result = client.get()
+    assert (phrase == result), f"Expected phrase retrieved {repr(result)} to be same as submitted {repr(phrase)}."
+    print(result, flush=True)
+    print()
+
+    # Try sending the opposite direction.
+    print("Sending from client to server..", flush=True)
+    phrase = "world hello!"
+    client.put(phrase)
+    result = server.get()
+    assert (phrase == result), f"Expected phrase retrieved {repr(result)} to be same as submitted {repr(phrase)}."
+    print(result, flush=True)
+    print()
+
+    print("server: ", server, flush=True)
+    print("client: ", client, flush=True)
