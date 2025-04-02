a3db3d2a928405b1fbfebf0ffd64f882c0e6878f
Thomas Lux <thomas.ch.lux@gmail.com>
2025-04-01 22:52:07 -0700

    Updated embedding client typing and added a single-embedding fetch option.

--- tlux/embedding/client.py
+++ tlux/embedding/client.py
@@ -9,23 +9,31 @@ embedding_queue = NetworkQueue(host="Macsey.local", port=12345, listen_port=None
 
 # Get embeddings for a list of strings.
 @cache()
-def get_embeddings(list_of_strings, batch_size=100):
+def get_embeddings(list_of_strings: list[str], batch_size: int=100):
     # Iterate over source data and generate embeddings.
-    embeddings = []
+    embeddings: list[list[np.ndarray]] = []
     for batch_i in tqdm(
         range(0, len(list_of_strings), batch_size),
-        total=len(list_of_strings) // batch_size
+        total=len(list_of_strings) // batch_size,
+        delay=5,
     ):
         samples = list_of_strings[batch_i : batch_i + batch_size]
         # Check remaining size of samples needed to be embedded.
-        if (len(samples) == 0):
-            print(f" skipping empty batch at range [{batch_i}, {batch_i+batch_size-1}]", flush=True)
-            continue
-        elif (len(samples) < batch_size):
-            print(f" only processing {len(samples)} from batch of {batch_size}..", flush=True)
+        if (len(list_of_strings) >= batch_size):
+            if (len(samples) == 0):
+                print(f" skipping empty batch at range [{batch_i}, {batch_i+batch_size-1}]", flush=True)
+                continue
+            elif (len(samples) < batch_size):
+                print(f" only processing {len(samples)} from batch of {batch_size}..", flush=True)
         # Generate embeddings.
         embedding_queue.put(samples)
         embeddings.append(embedding_queue.get())
     # Return the embeddings as a numpy matrix.
     embeddings = np.concat(embeddings, axis=0)
     return embeddings
+
+
+# Get embedding for a single of string.
+@cache()
+def get_embedding(string: str):
+    return get_embeddings([string])[0]
