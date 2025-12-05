7ce430485f2d73da31bbe7c0201b8fd37c799674
Thomas Lux <thomas.ch.lux@gmail.com>
2025-12-04 22:29:11 -0800

    Deleting files that should not exist and renaming 'utils' to 'libs' to clarify that it is effectively external code.

--- tlux/search/hkm/todo.py
+++
@@ -1,5 +0,0 @@
-# Support hash bit mask at build and search time.
-# Support arbitrary metadata columns instead of fixed schema.
-# Make the indexer take as an argument the metadata columns, numeric value ranges, and number of unique categorical values.
-# 
-# Pack multiple files into single doc shard.
